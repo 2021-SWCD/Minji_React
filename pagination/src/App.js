59c0d52b22609e89e4864b80d6f61e3ee70a1ad3
minj-L <wonder_minji@naver.com>
2021-08-03 11:00:37 +0900

    구현해놓은 파일 옮겨 담음

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navigator from "./components/navigator/Navigator";
+import Page from "./components/pagination/Page";
+import "./App.css";
 
-function App() {
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      {/* className은 단순히 클래스 명이다 */}
+      <h1 className="App_classTitle mb-3">CITZEN</h1>
+      <Navigator />
+      <Page />
     </div>
   );
-}
-
-export default App;
+};
